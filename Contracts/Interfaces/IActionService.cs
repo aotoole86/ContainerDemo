749079a7ee3346774418024a383d0a3a1db7922f
Austin O'Toole <austinotoole@hotmail.com>
2025-04-18 21:56:49 -0400

    init commit -

---
+++ Contracts/Interfaces/IActionService.cs
@@ -0,0 +1,23 @@
+﻿using ContainerDemo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerDemo.Contracts.Interfaces
+{
+    public interface IActionService
+    {
+        Invoice CreateInvoice(int containerId, DateTime valueDate, DateTime dueDate, string currency, decimal amount);
+        //Container CreateContainer(string name, DateTime startDate, DateTime startTime, DateTime endDate, DateTime endTime, string currency, decimal minTenor, decimal maxTenor, decimal maxAmount);
+        Container CreateContainer(string name, DateTime startDate, DateTime startTime, DateTime endDate, DateTime endTime, Dictionary<string, decimal> currencyMaxAmount, decimal minTenor, decimal maxTenor);
+        List<Container> GetContainers();
+        List<Invoice> GetInvoices();
+        void ProcessInvoicesBasic(List<Container> containers, List<Invoice> invoices);
+        void ProcessInvoicesTenor(List<Container> containers, List<Invoice> invoices);
+        void ProcessInvoicesBalances(List<Container> containers, List<Invoice> invoices);
+        void ProcessInvoicesMultipleCurrencies(List<Container> containers, List<Invoice> invoices);
+
+    }
+}
---
+++ Domain/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerDemo.Domain
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime ValueDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Currency { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
